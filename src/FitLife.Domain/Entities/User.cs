ba32c855e63a68d9fcd525702913834b4f93c1b4
GuilhermeLopesBertacini <guilb2005@gmail.com>
2025-11-17 21:24:31 -0300

    Add User entity with properties for Id, Name, Email, PasswordHash, and CreatedAt

---
+++ src/FitLife.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitLife.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+
+        public required string PasswordHash { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
